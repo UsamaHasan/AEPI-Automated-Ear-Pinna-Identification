9be5e5b41c3f10f4b9d3da0319a01ec9ed839ea8
Usama Hasan <usamahasan72@gmail.com>
2020-11-04 17:22:59 +0500

    Update models.py

--- src/models.py
+++ src/models.py
@@ -1,59 +1,6 @@
 import torch.nn as nn
 import torch
 
-class ArcMarginProduct(nn.Module):
-    """Implement of large margin arc distance: :
-        Args:
-            in_features: size of each input sample
-            out_features: size of each output sample
-            s: norm of input feature
-            m: margin
-            cos(theta + m)
-        """
-    def __init__(self, in_features, out_features, s=30.0, m=0.50, easy_margin=False):
-        super(ArcMarginProduct, self).__init__()
-        self.in_features = in_features
-        self.out_features = out_features
-        self.s = s
-        self.m = m
-        self.weight = nn.Parameter(torch.FloatTensor(out_features, in_features))
-        nn.init.xavier_uniform_(self.weight)
-
-        self.easy_margin = easy_margin
-        self.cos_m = math.cos(m)
-        self.sin_m = math.sin(m)
-        self.th = math.cos(math.pi - m)
-        self.mm = math.sin(math.pi - m) * m
-
-    def forward(self, input, label):
-        # --------------------------- cos(theta) & phi(theta) ---------------------------
-        #device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        
-        input = input.squeeze()
-        normalized_input =F.normalize(input)
-        normalized_weights = F.normalize(self.weight)
-        cosine = F.linear(normalized_input,normalized_weights )
-        sine = torch.sqrt((1.0 - torch.pow(cosine, 2)).clamp(0, 1))
-        phi = cosine * self.cos_m - sine * self.sin_m
-        if self.easy_margin:
-            phi = torch.where(cosine > 0, phi, cosine)
-        else:
-            phi = torch.where(cosine > self.th, phi, cosine - self.mm)
-        # --------------------------- convert label to one-hot ---------------------------
-        one_hot = torch.zeros(cosine.size(), device='cuda')
-        #one_hot = torch.zeros(cosine.size(), device=device)
-        try:
-            one_hot.scatter_(1, label.view(-1, 1).long(), 1)
-        except:
-            riase (f'Error Line')
-        # -------------torch.where(out_i = {x_i if condition_i else y_i) -------------
-        output = (one_hot * phi) + ((1.0 - one_hot) * cosine)  # you can use torch.where if your torch.__version__ is 0.4
-        output *= self.s
-        print(output.size())
-        return output
-
-
-
 class Net(nn.Module):
     """"""
     def __init__(self):
@@ -81,15 +28,15 @@ class Net(nn.Module):
         x = self.fc3(x)
         return x
     
-    def separable_conv_block(self):
+    def spatial_encoder_block(self):
         return torch.nn.Sequential( nn.Conv2d(3,64,(1,3),stride=3,groups=1),
-                             nn.PReLU(),
+                             nn.ReLU(),
                              nn.Conv2d(64,128,(3,1),stride=3,groups=64),
-                             nn.PReLU(),
+                             nn.ReLU(),
                              nn.Conv2d(128,256,(1,3),stride=3,groups=1),
-                             nn.PReLU(),
+                             nn.ReLU(),
                              nn.Conv2d(256,512,(3,1),stride=3,groups=256),
-                             nn.PReLU())
+                             nn.ReLU())
     
     def spatial_pyramid_pool(self,previous_conv, previous_conv_size, mode ):
         """
